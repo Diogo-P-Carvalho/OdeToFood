2a2e69e54c526674c5d6bc1954b5da6ca7199ea6
Diogo <dpcarvalho86@gmail.com>
2019-11-29 23:24:21 +0000

    Entity Framework

---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext dbContext;
+
+        public SqlRestaurantData(OdeToFoodDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        public void Add(Restaurant restaurant)
+        {
+            dbContext.Restaurants.Add(restaurant);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = dbContext.Restaurants.Find(id);
+            if (restaurant != null)
+            {
+                dbContext.Restaurants.Remove(restaurant);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public Restaurant Get(int id)
+        {
+            return dbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in dbContext.Restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            // Saves the last user update
+            //var r = Get(restaurant.Id);
+            //r.Name = restaurant.Name;
+            //r.Cuisine = restaurant.Cuisine;
+            //dbContext.SaveChanges();
+
+            // Optimistic cuncurrency 
+            var entry = dbContext.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+    }
+}
--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -15,9 +15,14 @@ namespace OdeToFood.Web
 
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
-            builder.RegisterType<InMemoryRestaurantData>()
+            //builder.RegisterType<InMemoryRestaurantData>()
+            //    .As<IRestaurantData>()
+            //    .SingleInstance();
+            builder.RegisterType<SqlRestaurantData>()
                 .As<IRestaurantData>()
-                .SingleInstance();
+                .InstancePerRequest();
+            builder.RegisterType<OdeToFoodDbContext>()
+                .InstancePerRequest();
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
